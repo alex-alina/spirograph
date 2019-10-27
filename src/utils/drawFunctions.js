6e599cdebd6817ac2f7fbabee26c6461239644a1
Alina <alex.alina.rusu@gmail.com>
2019-10-27 20:32:01 +0100

    Build basic spirograph functionality

--- src/utils/drawFunctions.js
+++ src/utils/drawFunctions.js
@@ -1,51 +1,77 @@
 import { Point, Path, Shape } from 'paper';
 
-
-let ellipse = null;
-
-// let shape = null;
-
-// const rotateLazyElipse = () => {
-//   if(shape === null) {
-//     shape = new Shape.Ellipse({
-//       point: [80, 250],
-//       size: [180, 60],
-//       fillColor: 'yellow'
-//   });
-//   }
-// }
+let smallCircle = null;
+let dot = null;
+let R = 280;
+let r = 100;
+const Cx = 300;
+const Cy = 300;
+const f = -1.2;
 
 const lazyInstantiate = () => {
-  if (ellipse === null) {
-    ellipse = new Shape.Ellipse({
-      point: [80, 250],
-      size: [40, 50],
-      strokeColor: 'green'
-});
+  if (smallCircle === null) {
+    smallCircle = new Path.Circle({
+      center: [10, 10],
+      radius: r,
+      strokeColor: 'green',
+      strokeWidth: 1
+    });
+    new Path.Circle({
+      center: [Cx, Cy],
+      radius: R,
+      strokeColor: 'blue',
+      strokeWidth: 1
+    });
+
+    const dotCenterX = Cx + R - r * (1 - f);
+    dot = new Path.Circle({
+      center: [dotCenterX, Cy],
+      radius: 3,
+      fillColor: 'black'
+    })
   }
 }
 
+
 export const draw = (event) => {
   lazyInstantiate();
-  // rotateLazyElipse();
-    ellipse.rotate(2);
-    // rectangle.position.x += event.delta*40;
-    // rectangle.position.y += event.delta*120;
+  // rectangle.position.x += event.delta*40;
+  // rectangle.position.y += event.delta*120;w
+  // smallCircle.rotate(2);
  const angle = event.time;
-    ellipse.position.x = Math.cos(angle) * 200 + 300;
-    ellipse.position.y = Math.sin(angle) * 200 + 300;
-    // if(rectangle.position.y > 600) {
-    //   rectangle.position.y = 0;
-    // }
-    // if(rectangle.position.x > 600) {
-    //   rectangle.position.x = 0;
-    // };
-    
-    // shape.rotate(4)
-  
-  }
+ const positionX = Math.cos(angle) * (R-r) + Cx;
+ const positionY = Math.sin(angle) * (R-r) + Cy;
+
+ smallCircle.position.x = positionX;
+ smallCircle.position.y = positionY;
+ 
+ const smallAngle = - angle * (R/r);
+ const pX = Math.cos(smallAngle) * r * f + positionX;
+ const pY = Math.sin(smallAngle) * r * f + positionY;
+
+ const path = new Path();
+		path.strokeColor = 'black';
+		// const start = new Point(100, 100);
+		path.moveTo(dot.position);
+		path.lineTo([pX, pY]);
+
+ dot.position.x = pX;
+ dot.position.y = pY;
+}
 
 
 
+//  dot.position.x = Math.cos(angle) * 170 + 300;
+//  dot.position.y = Math.sin(angle) * 210 + 300;
 
-  
+//  if(dot.position.x > 250) {
+//    dot.position.x = 210
+//  }
+
+//  if(dot.position.y > 250) {
+//    dot.position.y = 210
+//  }
+
+
+
+  	
